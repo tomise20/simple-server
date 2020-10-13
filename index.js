59c9cc5f517f84a4d0b4a5f36c72ac58e531d190
tomise20 <vira.tamas97@gmail.com>
2020-10-13 09:05:01 +0200

    update post method

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 "use strict";
 
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+let jsonParser = bodyParser.json();
 const port = 3000;
+
+let urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.get("/", (req, res) => {
     console.log(req.url);
@@ -13,6 +17,31 @@ app.get("/test", (req, res) => {
     let test = {title: "test"};
 
     res.status(200).send(test);
+})
+.post("/test", urlencodedParser, (req, res) => {
+    res.setHeader('Content-type', 'application/json');
+
+    if(req.headers["content-type"] !== 'application/json') {
+        res.statusCode = 400;
+        res.write(JSON.stringify({
+            code: 400,
+            data: null,
+            error: {
+                message: 'Bad Request!'
+            }
+        }));
+    } else {
+        console.log(req.body);
+        res.statusCode = 201;
+        res.write(JSON.stringify({
+            code: 201,
+            data: {
+                message: 'success request'
+            },
+            error: null
+        }));
+    }
+    res.send();
 });
 
 
