e5cf517158c77fa4b3981f3b746046473ea6a648
Tomi <developer@livestudio.eu>
2020-11-09 08:19:47 +0100

    update delete method and add put method

--- index.js
+++ index.js
@@ -8,74 +8,106 @@ const port = 3000;
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-    res.send("Hello");
+	res.send("Hello");
 });
 
 app.get("/test", (req, res) => {
-    let resData = {
-        error: null,
-        data: {
-            message: "Success message!"
-        }
-    };
+	let resData = {
+		error: null,
+		data: {
+			message: "Success message!",
+		},
+	};
 
-    res.setHeader('Content-type', 'application/json');
-    res.statusCode = 200;
-    res.write(JSON.stringify(resData));
-    res.send();
+	res.setHeader("Content-type", "application/json");
+	res.statusCode = 200;
+	res.write(JSON.stringify(resData));
+	res.send();
 })
-.post("/test", (req, res) => {
-    res.setHeader('Content-type', 'application/json');
+	.post("/test", (req, res) => {
+		res.setHeader("Content-type", "application/json");
 
-    if(req.headers["content-type"] !== 'application/json') {
-        res.statusCode = 400;
-        res.write(JSON.stringify({
-            data: null,
-            error: {
-                message: 'Bad Request!'
-            }
-        }));
-    } else {
-        res.statusCode = 201;
-        res.write(JSON.stringify({
-            data: {
-                message: 'Success request!'
-            },
-            error: null
-        }));
-    }
-    res.send();
-})
-.delete("/test", (req, res) => {
-    res.setHeader('Content-type', 'application/json');
+		if (req.headers["content-type"] !== "application/json") {
+			res.statusCode = 400;
+			res.write(
+				JSON.stringify({
+					data: null,
+					error: {
+						message: "Bad Request!",
+					},
+				})
+			);
+		} else {
+			res.statusCode = 201;
+			res.write(
+				JSON.stringify({
+					data: {
+						message: "Success request!",
+					},
+					error: null,
+				})
+			);
+		}
+		res.send();
+	})
+	.delete("/delete/:id", (req, res) => {
+		res.setHeader("Content-type", "application/json");
 
-    if(req.headers["content-type"] !== 'application/json') {
-        res.statusCode = 400;
-        res.write(JSON.stringify({
-            code: 400,
-            data: null,
-            error: {
-                message: 'Bad Request!'
-            }
-        }));
-    } else {
-        res.statusCode = 200;
-        res.write(JSON.stringify({
-            data: {
-                message: 'Success request!'
-            },
-            error: null
-        }));
-    }
-    res.send();
-});
+		if (req.headers["content-type"] !== "application/json") {
+			res.statusCode = 400;
+			res.write(
+				JSON.stringify({
+					code: 400,
+					data: null,
+					error: {
+						message: "Bad Request!",
+					},
+				})
+			);
+		} else {
+			res.statusCode = 200;
+			res.write(
+				JSON.stringify({
+					data: {},
+					message: "Successfully delete request!",
+					error: null,
+				})
+			);
+		}
+		res.send();
+	})
+	.put("/put/:id", (req, res) => {
+		res.setHeader("Content-type", "application/json");
 
+		if (req.headers["content-type"] !== "application/json") {
+			res.statusCode = 400;
+			res.write(
+				JSON.stringify({
+					code: 400,
+					data: null,
+					error: {
+						message: "Bad Request!",
+					},
+				})
+			);
+		} else {
+			res.statusCode = 200;
+			res.write(
+				JSON.stringify({
+					data: {},
+					message: "Successfully delete request!",
+					error: null,
+				})
+			);
+		}
+		res.send();
+	});
 
 app.listen(port, (err) => {
-    if(err) {
-        console.log("there was a problem", err);
-        return;
-    }
+	if (err) {
+		console.log("there was a problem", err);
+		return;
+	}
 
-    console.log(`listening on port ${port}`);
-})
+	console.log(`listening on port ${port}`);
+});
